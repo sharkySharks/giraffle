5ddb1b199e2fc0ffcce3a47b63ebc84ecc262613
Sharky <kaydenalthen@gmail.com>
2017-05-27 19:01:36 -0500

    feat(validation & create service): add form validation and create service

--- src/app/components/create/create.controller.js
+++ src/app/components/create/create.controller.js
@@ -1,15 +1,27 @@
-(function () {
+(function() {
     'use strict';
 
     angular
         .module('raffleApp')
         .controller('CreateController', CreateController)
 
-        function CreateController () {
+        CreateController.$inject = ['CreateRaffleSvc'];
+
+        function CreateController (CreateRaffleSvc) {
             var vm = this;
 
             vm.raffleName = '';
 
+            vm.submit = submit;
 
+            function submit () {
+                return CreateRaffleSvc.create(vm.raffleName).then(function (data) {
+                    // create url from data response
+                    // then navigate to that using $location.path('/new-raffle-path')
+                    vm.data = data;
+                }, function (err) {
+                    // return error to the user and ask them to try again
+                });
+            }
         }
 })()
